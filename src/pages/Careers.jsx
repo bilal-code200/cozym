8cbfa5e6e8dec14b67b0607eef97eadf915e432f
bilal-code200 <bilalmuhammed4300@gmail.com>
2025-12-22 22:20:36 +0100

    careers

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -39,33 +39,33 @@ export default function Careers() {
 	}, []);
 
 	return (
-		<div className="min-h-screen bg-white max-w-[1320px] mx-auto font-Montserrat ">
+		<div className="min-h-screen bg-white max-w-[1320px] mx-auto font-Montserrat px-4 md:px-[60px]">
 			<CareerH />
-			<div className="max-w-[50%] mt-[111px]">
-				<h1 className="text-[30px] leading-[42px] text-[#002B45] font-extrabold ">
+			<div className="w-full md:max-w-[50%] mt-16 md:mt-[111px]">
+				<h1 className="text-2xl md:text-[30px] leading-[42px] text-[#002B45] font-extrabold">
 					Build a future you can feature in
 				</h1>
-				<p className="text-[16px] leading-[30px] text-[#002B45] pt-[18px]">
+				<p className="text-sm md:text-[16px] leading-[30px] text-[#002B45] pt-[18px]">
 					Join a team of passionate engineers, project managers, and innovators
 					driving the future of energy infrastructure in Nigeria and West
 					Africa. Explore our current job openings below.
 				</p>
 			</div>
-			<div className="max-w-[50%] mt-[90px]">
-				<h1 className="text-[30px] leading-[42px] text-[#002B45] font-extrabold ">
+			<div className="w-full md:max-w-[50%] mt-12 md:mt-[90px]">
+				<h1 className="text-2xl md:text-[30px] leading-[42px] text-[#002B45] font-extrabold">
 					Open roles
 				</h1>
-				<p className="text-[16px] leading-[30px] text-[#002B45] pt-[18px]">
+				<p className="text-sm md:text-[16px] leading-[30px] text-[#002B45] pt-[18px]">
 					We are always happy to receive open applications, even if we are not
 					actively hiring.
 				</p>
 			</div>
 
-			<div className="grid grid-cols-1 md:grid-cols-12 gap-6 mt-[90px]">
-				<h2 className="col-span-3 text-[30px] leading-[42px] font-extrabold text-[#002B45]">
+			<div className="md:grid grid-cols-1 md:grid-cols-12 gap-6 mt-12 md:mt-[90px] hidden">
+				<h2 className="col-span-3 text-2xl md:text-[30px] leading-[42px] font-extrabold text-[#002B45]">
 					Job Title
 				</h2>
-				<h2 className="col-span-6 text-[30px] leading-[42px] font-extrabold text-[#002B45]">
+				<h2 className="col-span-6 text-2xl md:text-[30px] leading-[42px] font-extrabold text-[#002B45]">
 					Description
 				</h2>
 			</div>
@@ -76,20 +76,26 @@ export default function Careers() {
 				jobs.map((job, index) => (
 					<div
 						key={index}
-						className="grid grid-cols-1 md:grid-cols-12 gap-6 items-center mt-10 mb-[110px]"
+						className="grid grid-cols-1 md:grid-cols-12 lg:gap-6 gap-4 items-center mt-10 mb-16 md:mb-[110px]"
 					>
 						{/* Job Title */}
 						<div className="col-span-3">
-							<p className="text-[#FAA419] text-[16px]  font-medium">
+							<h2 className="col-span-3 md:hidden block text-2xl md:text-[30px] leading-[42px] font-extrabold text-[#002B45]">
+								Job Title
+							</h2>
+							<p className="text-[#FAA419] text-[16px] md:text-[16px] font-medium">
 								{job.Type}
 							</p>
-							<p className="text-[#002B45] text-[16px] font-bold">
+							<p className="text-[#002B45] text-sm md:text-[16px] mt-1 font-bold">
 								{job.Title}
 							</p>
 						</div>
 
 						{/* Job Description */}
-						<div className="col-span-6 text-sm text-[#002B45] text-[16px] leading-[30px] font-medium">
+						<div className="col-span-6 text-[#002B45] text-sm md:text-[16px] md:mt-0 mt-5  leading-[30px] font-medium">
+							<h2 className="col-span-6 md:hidden block text-2xl md:text-[30px] leading-[42px] font-extrabold text-[#002B45]">
+								Description
+							</h2>
 							<p>{job.Description}</p>
 							<p className="mt-1 text-[#F4A300]">
 								Location: {job.Location} | Experience: {job.Experience} |
@@ -98,10 +104,10 @@ export default function Careers() {
 						</div>
 
 						{/* Apply Button */}
-						<div className="col-span-3 flex justify-start md:justify-end">
+						<div className="col-span-3 flex justify-start md:justify-end mt-4 md:mt-0">
 							<button
 								onClick={() => window.open(job.FormLink, '_blank')}
-								className="bg-[#002B45] text-white px-8 py-2 rounded-lg text-sm hover:bg-[#014166] transition"
+								className="bg-[#002B45] text-white px-6 md:px-8 md:py-2 py-3 rounded-lg text-sm hover:bg-[#014166] transition w-1/2 md:w-auto"
 							>
 								Apply
 							</button>
